6667902b21725a1812d5bf3ee11f703321fb5de2
MANASRANJAN.DHAL <manasranjan.dhal@Manas-MacBook-Pro.local>
2020-09-22 11:28:12 +0530

    third commit

--- src/main/java/com/ci/cd/SpringJenkinApplication.java
+++ src/main/java/com/ci/cd/SpringJenkinApplication.java
@@ -2,12 +2,20 @@ package com.ci.cd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Success message";
 	}
 
 }
