7f94d6f1b18c49bac7c4d18e403fba6519b66f6c
MANASRANJAN.DHAL <manasranjan.dhal@Manas-MacBook-Pro.local>
2020-09-22 11:16:43 +0530

    second commit

---
+++ src/test/java/com/ci/cd/SpringJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
